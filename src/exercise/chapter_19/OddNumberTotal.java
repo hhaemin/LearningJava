29316375f28a1aca99023c4cb684431e2a3989c0
hhaemin <ahrgoals@naver.com>
2024-07-29 12:17:22 +0900

    홀수만 더하기

---
+++ src/exercise/chapter_19/OddNumberTotal.java
@@ -0,0 +1,16 @@
+package exercise.chapter_19;
+
+public class OddNumberTotal {
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if(i%2==0) {
+                continue;
+            }
+            System.out.println(i);
+            total += i;
+        }
+        System.out.printf("0~10까지 홀수를 더한 값은 %d",total);
+    }
+}
