2f58a0110220a1a15d6cea07310f6579f06fd80b
hhaemin <ahrgoals@naver.com>
2024-07-29 11:39:08 +0900

    단항연산자, 복합연산자

---
+++ src/exercise/chapter_18/Operator.java
@@ -0,0 +1,16 @@
+package exercise.chapter_18;
+
+public class Operator {
+    public static void main(String[] args) {
+
+        // += *=
+        int myInt = 5;
+        myInt +=1;
+        System.out.println(myInt); // 6
+
+        // 단항 연산자 ++
+        int myInt2 = 10;
+        myInt2++;
+        System.out.println(myInt2); // 1
+    }
+}
