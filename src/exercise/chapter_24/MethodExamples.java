c0dbb7638d8289784031111c3909879dc471e7fd
hhaemin <ahrgoals@naver.com>
2024-08-01 17:55:03 +0900

    여러가지 JAVA 함수 만들기

---
+++ src/exercise/chapter_24/MethodExamples.java
@@ -0,0 +1,48 @@
+package exercise.chapter_24;
+
+public class MethodExamples {
+
+    // 일반적인 입력값, 출력값도 있는 경우
+    // 둘 다 하나인 경우
+
+     static int toUnicode(char ch){
+         return (int) ch;
+     }
+
+     //
+     static int sumAndMultiply(int a, int b){
+         return (a + b) * 4;
+     }
+
+     public static void main(String[] args) {
+         char ch = 'A';
+         int decode = toUnicode(ch);
+         System.out.println(decode); // 65
+
+         int myInt1 = 10;
+         int myInt2 = 5;
+         int result = sumAndMultiply(myInt1, myInt2);
+         System.out.println(result); // 60
+
+         String res = sayHello();
+         System.out.println(res);
+
+         printHello("printHello");
+         printHi();
+     }
+
+     // 인자값이 없을 수 있다.
+    static String sayHello(){
+         return "Hello";
+    }
+
+    // 인자값이 있는데, 출력값은 없는 것.
+    static void printHello(String str){
+        System.out.printf("함수 안에서 실행합니다. %s\n", str);
+    }
+
+    // 인자값도 없고, 출력값도 없는 것.
+    static void printHi(){
+        System.out.println("Hi");
+    }
+}
