a74d28e29cef0fc02ed86e3c920c2a20d1ae0443
hhaemin <ahrgoals@naver.com>
2024-08-03 02:06:14 +0900

    Person으로 'this'를 반환하는 함수

--- src/exercise/chapter_28/ThisTest.java
+++ src/exercise/chapter_28/ThisTest.java
@@ -8,8 +8,17 @@ public class ThisTest {
         Person person2 = new Person("이민호","Male");
         Person person3 = new Person("아이유","Female",30);
 
-        person1.showMyself();
-        person2.showMyself();
-        person3.showMyself();
+//        person1.showMyself();
+//        person2.showMyself();
+//        person3.showMyself();
+//        Person 인스턴스: name: 강철중, gender: unknown, age: -1
+//        Person 인스턴스: name: 이민호, gender: Male, age: -1
+//        Person 인스턴스: name: 아이유, gender: Female, age: 30
+
+        Person person11 = person1.returnMySelf();
+        Person person22 = person2.returnMySelf();
+
+        System.out.println(person11); // exercise.chapter_28.Person@a09ee92
+        System.out.println(person22); // exercise.chapter_28.Person@30f39991
     }
 }
