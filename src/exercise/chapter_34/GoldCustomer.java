7fd6101bdb767676e25e61fd1b276603926bc8a1
hhaemin <ahrgoals@naver.com>
2024-08-08 23:58:32 +0900

    백화점 고객 관리 시스템

---
+++ src/exercise/chapter_34/GoldCustomer.java
@@ -0,0 +1,29 @@
+package exercise.chapter_34;
+
+public class GoldCustomer extends Customer {
+    // 속성
+    private double discountRatio;
+
+    // 행위
+    @Override
+    public int calculatePrice(int price){
+        this.bonusPoint += price * bonusPointRatio;
+        price -= price * discountRatio;
+        return price;
+    }
+
+    public GoldCustomer(String name){
+        super(); // 부모연산자에 무조건 디폴트생성자가 있어야 한다.
+        this.customerID = "Customer" + Customer.serialNums++; // customer와 ID를 같이 쓰기 때문에 VIPCustomer와 다르게 이렇게 표현
+        this.name = name;
+        this.customerGrade = "GOLD";
+        this.bonusPoint = 0;
+        this.bonusPointRatio = 0.03;
+        this.discountRatio = 0.03;
+    }
+
+    @Override
+    void printMyInfo(){
+        super.printMyInfo();
+    }
+}
