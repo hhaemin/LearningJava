a3b9a3d51f4e79eef009f06dd3b03de62800e0ea
hhaemin <ahrgoals@naver.com>
2024-08-19 11:16:32 +0900

    try-catch

---
+++ src/exercise/chapter_50/ExceptionTest.java
@@ -0,0 +1,29 @@
+package exercise.chapter_50;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExceptionTest {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        System.out.println("메인메소드 실행합니다.");
+        makeCheckedException();
+        System.out.println("메인메소드 종료합니다.");
+    }
+
+    public static void makeUncheckException(){
+        int[] intArr = {1,2,3,4,5,6,7,8,9,10};
+        int index = 5;
+
+        System.out.println("값: " + intArr[index]);
+    }
+
+    public static void makeCheckedException() throws FileNotFoundException {
+        File file = new File("exercise/chapter_50/text.txt");
+        FileInputStream fs = new FileInputStream(file);
+
+        System.out.println("FS 실행됩니다.");
+    }
+}
