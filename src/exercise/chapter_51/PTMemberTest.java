f6d3efbbc133b2ba17854eb40e7015f815062233
hhaemin <ahrgoals@naver.com>
2024-08-19 18:26:25 +0900

    사용자 정의 예외 만들기

---
+++ src/exercise/chapter_51/PTMemberTest.java
@@ -0,0 +1,11 @@
+package exercise.chapter_51;
+
+public class PTMemberTest {
+
+    public static void main(String[] args) {
+        PTMember member = new PTMember("mincheol",170,70,"Male");
+
+        member.setID("392847597");
+        System.out.println(member);
+    }
+}
