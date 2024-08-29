8bae28a5b0d849d8219ad0243b82cf34907b58c7
hhaemin <ahrgoals@naver.com>
2024-08-29 16:31:11 +0900

    Gradle

---
+++ MyGradle/src/main/java/org/example/person/PersonDto.java
@@ -0,0 +1,93 @@
+package org.example.person;
+
+public class PersonDto {
+
+    private String name;
+    private int age;
+    private String address;
+    private String email;
+    private String phone;
+    private String gender;
+    private String nationality;
+    private String education;
+
+    public PersonDto() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDto{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", education='" + education + '\'' +
+                '}';
+    }
+}
